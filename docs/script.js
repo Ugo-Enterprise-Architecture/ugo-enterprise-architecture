ef7e9447d1d409d6b90a5dbf6e0ec8cea76edb94
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2025-08-19 08:52:55 +0200

    PWA improvements

--- docs/script.js
+++ docs/script.js
@@ -24,7 +24,7 @@ const translations = {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(function(registration) {
         console.log('Service Worker registered with scope:', registration.scope);
       })
